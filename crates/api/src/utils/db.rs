9fff63553138b909388bd01e4b34d52e0526bb11
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-04-21 19:55:48 +0900

    fix: データベースの接続方法を修正

--- crates/api/src/utils/db.rs
+++ crates/api/src/utils/db.rs
@@ -1,11 +1,20 @@
 use sqlx::{PgPool, postgres::PgPoolOptions};
 use std::env;
 
+fn database_url_from_env() -> String {
+    let user = env::var("POSTGRES_USER").unwrap();
+    let pass = env::var("POSTGRES_PASSWORD").unwrap();
+    let host = env::var("POSTGRES_HOST").unwrap();
+    let port = env::var("POSTGRES_PORT").unwrap();
+    let db   = env::var("POSTGRES_DATABASE").unwrap();
+
+    format!("postgres://{}:{}@{}:{}/{}", user, pass, host, port, db)
+}
+
 pub async fn connect_pg() -> PgPool {
-    let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgPoolOptions::new()
         .max_connections(5)
-        .connect(&url)
+        .connect(&database_url_from_env())
         .await
         .expect("Failed to connect to PostgreSQL")
 }
