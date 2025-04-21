69413f7768b6a5e4645220d90f9f8b492fe25a3f
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-04-21 18:24:18 +0900

    fix: 型修正

--- crates/api/src/models/items/base.rs
+++ crates/api/src/models/items/base.rs
@@ -8,11 +8,11 @@ pub struct Item {
     pub version: i64,
     pub name: String,
     pub lore: Vec<String>,
-    pub rarity: i8,
-    pub max_stack: i32,
+    pub rarity: i16,
+    pub max_stack: i16,
     pub custom_model_data: i32,
     pub price: Price,
-    pub data: ItemData,
+    pub data: serde_json::Value,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -22,15 +22,29 @@ pub struct Price {
     pub can_sell: bool,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(tag = "category", content = "data")]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum ItemData {
     Food(FoodData),
     Tool(ToolData),
     Armor(ArmorData),
 }
 
+impl std::fmt::Display for ItemCategory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ItemCategory::Food => "food",
+                ItemCategory::Tool => "tool",
+                ItemCategory::Armor => "armor",
+            }
+        )
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "lowercase")]
 pub enum ItemCategory {
     Food,
     Tool,
