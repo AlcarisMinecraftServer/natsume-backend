03ca27c949f73ac6ba52ed6bcab15e91b81050fd
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-05-06 23:39:00 +0900

    Revert "fix: table"
    This reverts commit 38bff59651ef8e07c72f2d430b69d18a2398623b.

--- migrations/20250420215834_create_items_table.sql
+++ migrations/20250420215834_create_items_table.sql
@@ -1,14 +1,13 @@
 -- Add migration script here
 CREATE TABLE items (
-    id                 TEXT        PRIMARY KEY,
-    version            BIGINT      NOT NULL,
-    name               TEXT        NOT NULL,
-    category           TEXT        NOT NULL CHECK (category IN ('food','tool','armor','weapon')),
-    lore               JSONB       NOT NULL,
-    rarity             SMALLINT    NOT NULL,
-    max_stack          SMALLINT    NOT NULL,
-    custom_model_data  INTEGER     NOT NULL,
-    price              JSONB       NOT NULL,
-    data               JSONB       NOT NULL,
-    tags               JSONB       NOT NULL DEFAULT '[]'::jsonb
+    id TEXT PRIMARY KEY,
+    version BIGINT NOT NULL,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL CHECK (category IN ('food', 'tool', 'armor')),
+    lore JSONB NOT NULL,
+    rarity SMALLINT NOT NULL,
+    max_stack SMALLINT NOT NULL,
+    custom_model_data INTEGER NOT NULL,
+    price JSONB NOT NULL,
+    data JSONB NOT NULL
 );
